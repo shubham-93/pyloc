d77da8c24c61becfca8086edd706a4fd4a0f3b9f
Shubham Maheshwari <shubham.93@gmail.com>
2024-09-15 19:26:18 +0200

    added a test file

---
+++ testfile.py
@@ -0,0 +1,70 @@
+# LOC: 8
+
+# This is a comment
+# Let's start with some whitespace lines below
+
+
+
+
+
+
+
+
+
+
+def foo():
+    pass
+
+
+
+def bar():
+    pass
+
+# This is a comment
+
+# This is another comment
+a = 1 # a is 1
+
+
+"""
+This is
+             a
+     multiline
+docstring
+"""
+
+# There are some whitespace lines below
+
+
+
+
+
+
+
+
+'''
+
+This is
+          also
+
+      a multiline
+
+   docstring
+
+
+
+'''
+
+b = 2 
+
+class Animal:
+    """This is an animal class"""
+    pass
+
+             # This is another comment with leading whitespace
+
+# More whitespace lines below        
+          
+
+
+
